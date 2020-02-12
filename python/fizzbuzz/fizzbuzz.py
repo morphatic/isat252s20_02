f18a1b987ecf4e4e24d6e0a961bcaf40f5fe5deb
Morgan Benton <registered@morphatic.com>
2020-02-12 11:07:14 -0500

    add fizz() test

---
+++ python/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,4 @@
+"""A FizzBuzz program"""
+
+def fizz(x):
+  return x % 3 == 0
--- python/fizzbuzz/fizzbuzz_test.py
+++ python/fizzbuzz/fizzbuzz_test.py
@@ -1,4 +1,17 @@
-def describe_a_fizzbuzz_program():
-  def test_smoke():
+# pylint: disable=unused-variable
+"""Unit tests for FizzBuzz."""
+
+# import the code to be tested
+from fizzbuzz import fizz
+
+def describe_a_fizzbuzz_program_that():
+  """A program to play the FizzBuzz game."""
+
+  def has_a_smoke_test():
     """Make sure our testing infrastructure is working."""
-    assert True == False
+    assert True == True
+
+  def determine_if_a_number_is_a_multiple_of_3():
+    """Checks to see if a number is a multiple of 3"""
+    assert fizz(3) == True
+    assert fizz(2) == False
