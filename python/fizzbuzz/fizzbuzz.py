f17ae8eff11e1a0b6abba77d5269af0575a99cb3
Morgan Benton <registered@morphatic.com>
2020-02-14 11:02:09 -0500

    refactor fizz test

--- python/fizzbuzz/fizzbuzz.py
+++ python/fizzbuzz/fizzbuzz.py
@@ -1,4 +1,13 @@
 """A FizzBuzz program"""
 
+# import necessary support classes
+from numbers import Number
+
 def fizz(x):
-  return x % 3 == 0
+  # check to make sure that x is numeric
+  if isinstance(x, Number):
+    # yes, it is a number
+    return x % 3 == 0
+  else:
+    # no, it is NOT a number
+    return False
--- python/fizzbuzz/fizzbuzz_test.py
+++ python/fizzbuzz/fizzbuzz_test.py
@@ -13,5 +13,9 @@ def describe_a_fizzbuzz_program_that():
 
   def determine_if_a_number_is_a_multiple_of_3():
     """Checks to see if a number is a multiple of 3"""
-    assert fizz(3) == True
-    assert fizz(2) == False
+    assert fizz(3) == True       # multiple of 3
+    assert fizz(2) == False      # non-multiple of 3
+    assert fizz(0) == True       # zero
+    assert fizz(-3) == True      # negative multiple of 3
+    assert fizz(-4) == False     # negative non-multiple of 3
+    assert fizz('buzz') == False # non-number input
