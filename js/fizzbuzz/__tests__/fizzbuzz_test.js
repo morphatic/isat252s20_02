7eaa0b1e9f47072dc2b847c8cf0ca5e9eea47714
Morgan Benton <registered@morphatic.com>
2020-03-04 11:00:31 -0500

    working on fizz error test

--- js/fizzbuzz/__tests__/fizzbuzz_test.js
+++ js/fizzbuzz/__tests__/fizzbuzz_test.js
@@ -1,13 +1,16 @@
-
+// import the code to be tested
+//import { fizz } from '../fizzbuzz' // <-- ES2016+ way of importing
+const { fizz } = require('../fizzbuzz') // <-- how to import in "plain" javascript prior to ES2016
 
 describe('A FizzBuzz program', () => {
   it('has a smoke test', () => {
-    expect(true).toBe(false)
+    expect(true).toBe(true)
+    expect(false).not.toBe(true)
   })
 
   describe('A function fizz()', () => {
     it('throws an error if it has no input', () => {
-      expect(fizz()).toThrow()
+      expect(() => { fizz(3) }).toThrow('there is no input')
     })
   })
 })
---
+++ js/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,20 @@
+/**
+ * Checks to see if an input is numeric and a multiple of 3.
+ * Returns 'Fizz' if it is.
+ * Returns the input if it is not.
+ * 
+ * @param {number|string} x The input to be checked
+ */
+const fizz = function (x) {
+  // check to see if x exists/is defined
+  if (/* x exists */) {
+    // yes, it does, carry on
+  } else {
+    // no, it doesn't, throw an error
+    throw new Error('there is no input')
+  }
+}
+
+module.exports = {
+  fizz
+}
